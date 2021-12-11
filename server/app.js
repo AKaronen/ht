87cdab3777a7cd9d885446cb62eff4052fc925d6
Unlucky72 <alex.karonen@gmail.com>
2021-12-11 20:11:02 +0200

    joojoo

--- server/app.js
+++ server/app.js
@@ -1,12 +1,17 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require("mongoose");
+const cors = require("cors");
+const mongoDB = "mongodb://localhost:27017/projectdb" //process.env.MONGO_URL; 
+mongoose.connect(mongoDB);
+mongoose.Promise = Promise;
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error!"));
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -14,7 +19,19 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
-module.exports = app;
+
+
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.resolve("..", "client", "build")));
+    app.get("*", (req, res) =>{
+        res.sendFile(path.resolve("..", "client", "build", "index.html"))
+    });
+}else if(process.env.NODE_ENV === "development"){
+    var corsOptions = {
+        origin: "http://localhost:3000",
+        optionsSuccessStatus: 200,
+    };
+    app.use(cors(corsOptions));
+}
